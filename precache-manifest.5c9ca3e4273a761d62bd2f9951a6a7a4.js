9a1881f43eb5ceb6581984da622c31fed4a8d755
Carlos-Galv <carlosgalvantds@gmail.com>
2020-09-05 03:21:27 -0300

    Updates

---
+++ precache-manifest.5c9ca3e4273a761d62bd2f9951a6a7a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58315a2c17f0a6be4abbb0197513d7c8",
+    "url": "/Ecommerce/index.html"
+  },
+  {
+    "revision": "4cab0fa034ead013a3ae",
+    "url": "/Ecommerce/static/css/main.bd23edeb.chunk.css"
+  },
+  {
+    "revision": "1aaa5b5bf2649065c9d1",
+    "url": "/Ecommerce/static/js/2.bc83cfcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ecommerce/static/js/2.bc83cfcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cab0fa034ead013a3ae",
+    "url": "/Ecommerce/static/js/main.32d69a26.chunk.js"
+  },
+  {
+    "revision": "f3b846fb9224c098ce8f",
+    "url": "/Ecommerce/static/js/runtime-main.1f902644.js"
+  }
+]);
